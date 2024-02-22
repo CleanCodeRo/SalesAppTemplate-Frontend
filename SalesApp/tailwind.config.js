1b00fb304a087c5868bb5af757fdb8a17def8f50
Denisa Cuta <denisa1506@yahoo.com>
2024-02-22 11:36:18 +0200

    Colors

--- SalesApp/tailwind.config.js
+++ SalesApp/tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#665749",
+          100: "#106659",
+          200: "#13918d",
+          300: "#1caca8",
+          400: "#a29c8d",
+          500: "#c4c9b6",
+        },
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
-
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
